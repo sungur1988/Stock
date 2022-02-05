018f045b033827ec48b5875253a14056de4ad162
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-05 22:41:09 +0300

    IProductMovementServiceAdded

---
+++ Business/Abstract/IProductMovementService.cs
@@ -0,0 +1,18 @@
+﻿using Core.Utilities.Results;
+using Entities.Dtos;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductMovementService
+    {
+        IResult Add(ProductMovement productMovement);
+        IDataResult<IEnumerable<ProductMovement>> GetList(int userId);
+        IDataResult<IEnumerable<StockTotalDto>> GetStockTotals();
+        IDataResult<IEnumerable<ProductMovement>> GetListByProductId(int userId,int productId);
+
+    }
+}
