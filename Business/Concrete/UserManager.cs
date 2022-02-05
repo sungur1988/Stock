9ab5e1705b37b8deb14140c75b65c70d8ffd7644
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-05 22:42:18 +0300

    UserManagerAdded

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using Core.EntityLayer.Concrete.AuthorizationEntities;
+using DataAccess.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        private IUserRepository _userRepository;
+        public UserManager(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+        public void Add(User user)
+        {
+            _userRepository.Add(user);
+        }
+
+        public User GetByMail(string email)
+        {
+            return _userRepository.Get(x => x.Email == email);
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+            return _userRepository.GetClaims(user);
+        }
+    }
+}
