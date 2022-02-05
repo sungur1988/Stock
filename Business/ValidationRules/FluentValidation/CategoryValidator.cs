80357c981077b1361b170f9da1f427fa86d915af
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-05 22:54:20 +0300

    CategoryValidatorAdded

---
+++ Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CategoryValidator : AbstractValidator<Category>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(c => c.CategoryName).NotNull().MaximumLength(50);
+        }
+    }
+}
