a404d0eb23bb8515c02863e171f050e7dcfa2a13
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-05 22:41:18 +0300

    IProductServiceAdded

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Utilities.Results;
+using Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IResult Add(Product product);
+        IResult Update(Product product);
+        IResult Delete(Product product, int userId);
+        IDataResult<IEnumerable<Product>> GetListByCategoryId(int categoryId);
+        IDataResult<IEnumerable<Product>> GetList();
+    }
+}
