5f5a7a6632ca69dfe95775ba41fb0c3a712baf40
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-05 22:42:44 +0300

    MessagesModified

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -17,5 +17,9 @@ namespace Business.Constants
         public static string ProductMovementAdded = "Ürün hareketi başarılı bir şekilde eklendi.";
         public static string ProductMovementListed = "Ürün hareketleri listelendi.";
         public static string AuthorizationDenied = "Bu işlem için yetkiniz yok.";
+        public static string StockTotalListed= "Stoklar listelendi ";
+        public static string DifferentUserAddedCategory = "Bu kategori başka bir kullanıcı tarafından eklenmiştir.Silme yetkiniz yoktur.";
+        public static string ProductExistInThisCategory = "Bu kategoriye ait ürün bulunduğundan bu kategori silinemez.";
+        public static string ProductMovementExistWithThisProduct = "Bu ürüne ait stok hareketleri olduğu için bu ürün silinemez.";
     }
 }
