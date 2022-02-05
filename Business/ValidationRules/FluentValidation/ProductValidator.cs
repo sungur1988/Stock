df237e85bf001557987eccbae4289601f0ae3e9c
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-05 22:54:55 +0300

    ProductValidatorAdded

---
+++ Business/ValidationRules/FluentValidation/ProductValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ProductValidator :AbstractValidator<Product>
+    {
+        public ProductValidator()
+        {
+            RuleFor(x => x.CategoryId).NotNull();
+            RuleFor(x => x.ProductName).NotNull().MaximumLength(100);
+            RuleFor(x => x.Price).NotNull().GreaterThan(0);
+            RuleFor(p => p.CreatedUserId).NotNull();
+            RuleFor(p => p.CreatedDate).NotNull();
+        }
+    }
+}
