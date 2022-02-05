9f46e77fb5486d836646b8b6401e97e847aaed0f
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-05 22:40:57 +0300

    ICategoryServiceAdded

--- Business/Abstract/ICategoryService.cs
+++ Business/Abstract/ICategoryService.cs
@@ -11,6 +11,6 @@ namespace Business.Abstract
         IResult Add(Category category);
         IResult Update(Category category);
         IDataResult<IEnumerable<Category>> GetList(int userId);
-        IResult Delete(Category category);
+        IResult Delete(Category category,int userId);
     }
 }
