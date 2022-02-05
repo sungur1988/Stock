3e35e2bff1a74594095fde163386e28be3684d24
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-06 02:17:25 +0300

    IAuthServiceAdded

---
+++ Business/Abstract/IAuthService.cs
@@ -0,0 +1,19 @@
+﻿using Core.EntityLayer.Concrete.AuthorizationEntities;
+using Core.Utilities.Results;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IAuthService
+    {
+        IDataResult<User> Register(UserForRegisterDto userForRegisterDto);
+        IDataResult<User> Login(UserForLoginDto userForLoginDto);
+        IResult UserExist(string email);
+        IDataResult<AccessToken> CreateAccessToken(User user);
+
+    }
+}
