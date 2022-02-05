92a67440b7610eff27e0997022f2c5d6a8457496
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-06 02:17:34 +0300

    AuthManagerAdded

---
+++ Business/Concrete/AuthManager.cs
@@ -0,0 +1,72 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.EntityLayer.Concrete.AuthorizationEntities;
+using Core.Utilities.Results;
+using Core.Utilities.Security.HashHelpers;
+using Core.Utilities.Security.Jwt;
+using Entities.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class AuthManager : IAuthService
+    {
+        private IUserService _userService;
+        private ITokenHelper _tokenHelper;
+
+        public AuthManager(IUserService userService,ITokenHelper tokenHelper)
+        {
+            _userService = userService;
+            _tokenHelper = tokenHelper;
+        }
+        public IDataResult<AccessToken> CreateAccessToken(User user)
+        {
+            var claims = _userService.GetClaims(user);
+            var token = _tokenHelper.CreateToken(user, claims);
+            return new SuccessDataResult<AccessToken>(token, Messages.AccessTokenCreated);
+        }
+
+        public IDataResult<User> Login(UserForLoginDto userForLoginDto)
+        {
+            var userToCheck = _userService.GetByMail(userForLoginDto.Email);
+            if (userToCheck == null)
+            {
+                return new ErrorDataResult<User>(Messages.UserNotFound);
+            }
+            if (!HashingHelper.VerifyPasswordHash(userForLoginDto.Password,userToCheck.PasswordHash,userToCheck.PasswordSalt))
+            {
+                return new ErrorDataResult<User>(Messages.PasswordError);
+            }
+            return new SuccessDataResult<User>(userToCheck, Messages.SuccessfulLogin);
+        }
+
+        public IDataResult<User> Register(UserForRegisterDto userForRegisterDto)
+        {
+            byte[] passwordHash, passwordSalt;
+            HashingHelper.CreatePasswordHash(userForRegisterDto.Password, out passwordHash, out passwordSalt);
+            var createdUser = new User
+            {
+                FirstName = userForRegisterDto.FirstName,
+                LastName = userForRegisterDto.LastName,
+                Email = userForRegisterDto.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Status = true
+            };
+            _userService.Add(createdUser);
+            return new SuccessDataResult<User>(createdUser, Messages.UserRegistered);
+        }
+
+        public IResult UserExist(string email)
+        {
+    
+            if (_userService.GetByMail(email) != null)
+            {
+                return new ErrorResult(Messages.UserAlreadyExist);
+            }
+            return new SuccessResult();
+        }
+    }
+}
