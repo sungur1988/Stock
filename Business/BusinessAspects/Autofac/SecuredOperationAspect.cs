5416db47bb7fe427afead78f792365838f8e18ef
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-06 23:07:11 +0300

    SecuredOperationAspectAddedForAOPAuthorization

---
+++ Business/BusinessAspects/Autofac/SecuredOperationAspect.cs
@@ -0,0 +1,36 @@
+﻿using Business.Constants;
+using Castle.DynamicProxy;
+using Core.Extensions;
+using Core.Utilities.Interceptors;
+using Core.Utilities.IoC;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.BusinessAspects.Autofac
+{
+    public class SecuredOperationAspect : MethodInterception
+    {
+        private IHttpContextAccessor _contextAccessor;
+        private string[] _roles;
+        public SecuredOperationAspect(string roles )
+        {
+            _roles = roles.Split(",");
+            _contextAccessor = ServiceTool.ServiceProvider.GetService<IHttpContextAccessor>();
+        }
+        protected override void OnBefore(IInvocation invocation)
+        {
+            var roleClaims = _contextAccessor.HttpContext.User.FindClaimRoles();
+            foreach (var role in _roles)
+            {
+                if (roleClaims.Contains(role))
+                {
+                    return;
+                }
+            }
+            throw new Exception(Messages.AuthorizationDenied);
+        }
+    }
+}
