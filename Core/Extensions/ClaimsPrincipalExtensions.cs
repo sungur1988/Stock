5108f3cfcddf94f32b06e2b26ff35d6b8997daea
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-06 23:04:32 +0300

    ClaimsPrincipalExtensionsAdded

---
+++ Core/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static List<string> FindAllClaims(this ClaimsPrincipal claimprincipal,string claimType)
+        {
+            return claimprincipal?.FindAll(claimType)?.Select(x => x.Value).ToList();
+        }
+        public static List<string> FindClaimRoles(this ClaimsPrincipal claimsPrincipal)
+        {
+            return claimsPrincipal.FindAllClaims(ClaimTypes.Role);
+        }
+    }
+}
