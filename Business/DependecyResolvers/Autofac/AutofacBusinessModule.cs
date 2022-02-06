6b67a3a2f21c7e887caf4c73a533cca919f96617
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-06 19:15:50 +0300

    AutofacBusinessModuleConfiguredForAOP

--- Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
+++ Business/DependecyResolvers/Autofac/AutofacBusinessModule.cs
@@ -1,6 +1,9 @@
 ﻿using Autofac;
+using Autofac.Extras.DynamicProxy;
 using Business.Abstract;
 using Business.Concrete;
+using Castle.DynamicProxy;
+using Core.Utilities.Interceptors;
 using Core.Utilities.Security.Jwt;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFrameworkCore.Repositories;
@@ -28,6 +31,13 @@ namespace Business.DependecyResolvers.Autofac
 
             builder.RegisterType<AuthManager>().As<IAuthService>();
             builder.RegisterType<JwtHelper>().As<ITokenHelper>();
+
+            var assembly = System.Reflection.Assembly.GetExecutingAssembly();
+            builder.RegisterAssemblyTypes(assembly).AsImplementedInterfaces()
+                .EnableInterfaceInterceptors(new ProxyGenerationOptions()
+                {
+                    Selector = new AspectInterceptorSelector()
+                }).SingleInstance();
 
         }
     }
