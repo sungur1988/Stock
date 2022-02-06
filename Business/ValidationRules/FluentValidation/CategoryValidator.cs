a36533cb55542f910fff32a3ba76cea7d39fa4f3
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-06 19:16:21 +0300

    CategoryValidatorConfiguredForSingleResponsibility

--- Business/ValidationRules/FluentValidation/CategoryValidator.cs
+++ Business/ValidationRules/FluentValidation/CategoryValidator.cs
@@ -10,7 +10,8 @@ namespace Business.ValidationRules.FluentValidation
     {
         public CategoryValidator()
         {
-            RuleFor(c => c.CategoryName).NotNull().MaximumLength(50);
+            RuleFor(c => c.CategoryName).NotNull();
+            RuleFor(c => c.CategoryName).MaximumLength(50);
         }
     }
 }
