181fc9b6ad495c2b02c0e3cd7497d88ed8340132
sungur1988 <77548325+sungur1988@users.noreply.github.com>
2022-02-06 21:48:46 +0300

    MemoryCacheManagerAdded

---
+++ Core/CrossCuttingConcerns/Caching/Microsoft/MemoryCacheManager.cs
@@ -0,0 +1,44 @@
+﻿using Core.Utilities.IoC;
+using Microsoft.Extensions.Caching.Memory;
+using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Caching.Microsoft
+{
+    public class MemoryCacheManager : ICacheManager
+    {
+        private IMemoryCache _cache;
+        public MemoryCacheManager()
+        {
+             _cache = ServiceTool.ServiceProvider.GetService<IMemoryCache>();
+        }
+        public void Add(string key, object data, int duration)
+        {
+            _cache.Set(key, data, TimeSpan.FromMinutes(duration));
+        }
+
+        public T Get<T>(string key)
+        {
+            return _cache.Get<T>(key);
+        }
+
+        public object Get(string key)
+        {
+            return _cache.Get(key);
+        } 
+
+        public bool IsAdd(string key)
+        {
+            return _cache.TryGetValue(key, out _);
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+       
+    }
+}
